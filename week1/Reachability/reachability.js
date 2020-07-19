eeee14171f9186fe529d158376e2bd76aebbc2d1
Darko <dpranjic@iolap.com>
2020-07-19 18:20:08 +0200

    (fix): Reachability fix

--- week1/Reachability/reachability.js
+++ week1/Reachability/reachability.js
@@ -15,19 +15,19 @@ class Graph {
   }
 
   DFS(u, v) {
+    this.visited_vertices.set(u, true);
+
     if (this.adj[u - 1].includes(v)) {
       return 1;
     }
 
-    if (this.visited_vertices.get(u)) {
-      return 0;
-    }
-
-    this.visited_vertices.set(u, true);
-
     for (let i = 0; i < this.adj[u - 1].length; i++) {
-      return this.DFS(this.adj[u - 1][i], v);
+      if (!this.visited_vertices.get(this.adj[u - 1][i])) {
+        return this.DFS(this.adj[u - 1][i], v);
+      }
     }
+
+    return 0;
   }
 }
 
@@ -36,7 +36,9 @@ const graph1 = new Graph(4);
 graph1.addEdge(1, 2);
 graph1.addEdge(3, 2);
 graph1.addEdge(4, 3);
-graph1.addEdge(1, 4);
+// graph1.addEdge(1, 4);
+
+console.log(graph1);
 
 console.log(graph1.checkReachability(1, 4));
 
